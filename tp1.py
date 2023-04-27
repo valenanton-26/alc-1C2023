fad790f2aa42535c20ba11fe0fd8c7fe23f20866
valenanton-26 <81269848+valenanton-26@users.noreply.github.com>
2023-04-27 14:08:33 -0300

    Update tp1.py

--- tp1.py
+++ tp1.py
@@ -14,10 +14,11 @@ import math
 # potencia con un vector aleatorio inicial v ∈ Rn. El programa debe devolver un vector a ∈ Rk, donde ai sea la 
 # aproximacion al autovalor obtenida en el paso i.
 
-def producto_matriz_k_veces(A, k):
+
+def producto_matriz_k_veces(A, k): #se asume k>0
     B = A
     i = 1
-    while (i < k) :
+    while (i < k):  #B = A^i al finalizar cada iteración del ciclo. Por eso i<k
         B = B@A
         i = i+1
     return B;
@@ -25,7 +26,7 @@ def producto_matriz_k_veces(A, k):
 def cociente_rayleigh(x,A):
     num = x @ A @ x
     den = x @ x
-    return np.round(num/den)    
+    return (num/den)    
 
 def autovector_generado(matriz, vect):
     prod = matriz@vect
@@ -33,18 +34,26 @@ def autovector_generado(matriz, vect):
     norma = np.linalg.norm(prod, 2)
     
     return prod/norma
-    
 
 
-def generar_autovalor(M, v):
+def generar_autovalor(M, v, k):
     # el resultado de multiplicar k veces a la matriz A
-    B2 = producto_matriz_k_veces(M, 50)
+    B2 = producto_matriz_k_veces(M, k)
     # calculo del autovector
     autovector1 = autovector_generado(B2, v)
     
     a1 = cociente_rayleigh(autovector1 , M)
     return a1;
 
+def generar_vector(M, v, k):
+    a = []
+    i = 0 #cuenta iteraciones del ciclo
+    while i < k:
+        autovalor = generar_autovalor(M, v, i+1)
+        a = np.append(a, autovalor)
+        print(a)
+        i += 1
+    return a
 
 # A = matriz a la que le va a calcular los autovalores
 A = np.array([[-2.,0,0],[0,-5.,6.],[0,-3.,4.]])
@@ -52,7 +61,52 @@ A = np.array([[-2.,0,0],[0,-5.,6.],[0,-3.,4.]])
 # vector random de la longitud de los vectores de la matriz inicial
 v1 = np.random.rand(A.shape[0])
 
-print(generar_autovalor(A,v1))
+print(v1)
+print(generar_vector(A,v1,10))
 
 # para verificar
 print( np.linalg.eigvals(A))
+
+#EJERCICIO 2
+
+def generarMatrizTipoA(n):
+    A = np.random.randn(n,n)
+    # genera una matriz de elementos reales aleatorios, de forma nxn
+    return A
+
+def generarMatrizTipoB(n):
+    B = np.random.randn(n,n)
+    i  = 0
+    while i < (n-1):
+        j = i+1
+        while j < n:
+            B[i][j] = B[j][i]
+            j +=1
+        i +=1
+    return B
+
+def generarMatrizTipoC(M):
+    C = M.copy()
+    i = 0
+    while i < M.shape[0]:
+        C[i][i] = (C[i][i]) + 100
+        i += 1
+    return C
+
+def generarMatrizTipoD(M):
+    C = M.copy()
+    i = 0
+    while i < M.shape[0]:
+        C[i][i] = (C[i][i]) + 1000
+        i += 1
+    return C
+
+def generarMatrices(n):
+    A = generarMatrizTipoA(n)
+    B = generarMatrizTipoB(n)
+    C = generarMatrizTipoC(B)
+    D = generarMatrizTipoD(B)
+    return A, B, C, D
+
+# Item A
+
